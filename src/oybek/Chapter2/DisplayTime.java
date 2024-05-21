c881b18958eb69434e7b73697c33695b76b8d44f
power <x.oybek94@gmail.com>
2024-05-21 12:57:54 +0900

    add: DisplayTime

---
+++ src/oybek/Chapter2/DisplayTime.java
@@ -0,0 +1,18 @@
+package oybek.Chapter2;
+
+import java.util.Scanner;
+
+public class DisplayTime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer for seconds");
+        int seconds = input.nextInt();
+
+        int minutes = seconds / 60; //find minutes in seconds
+        int remainingSeconds = seconds % 60; //Seconds remaining
+
+        System.out.println(seconds + " seconds is " + minutes +
+                " minutes and " + remainingSeconds + " seconds");
+    }
+}
