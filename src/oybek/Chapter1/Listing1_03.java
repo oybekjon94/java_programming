1b096045d1ec10e1a9791d8ed158307eaa99a875
power <x.oybek94@gmail.com>
2024-05-21 11:56:27 +0900

    Listing1_03

---
+++ src/oybek/Chapter1/Listing1_03.java
@@ -0,0 +1,8 @@
+package oybek.Chapter1;
+
+public class Listing1_03 {
+    public static void main(String[] args) {
+        System.out.print("(10.5+2*3) / (54-3.5) = ");
+        System.out.println((10.5+2*3) / (54-3.5));
+    }
+}
