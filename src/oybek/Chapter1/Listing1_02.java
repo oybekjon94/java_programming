f2caa80b9eb0fdc8b78c965c82783e272c515363
power <x.oybek94@gmail.com>
2024-05-21 11:53:20 +0900

    Initial commit

---
+++ src/oybek/Chapter1/Listing1_02.java
@@ -0,0 +1,9 @@
+package oybek.Chapter1;
+
+public class Listing1_02 {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamental first");
+        System.out.println("Problem Driven");
+    }
+}
